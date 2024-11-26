8ec263c131950972a77da743249c900a868bcd60
unpowd4r <sergay.frolov@yandex.ru>
2024-11-26 15:54:06 +0400

    refactor component Tasks

--- src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,9 +1,7 @@
 import List from "@mui/material/List"
-import { TaskStatus } from "common/enums"
-import { useGetTasksQuery } from "features/todolists/api/tasksApi"
+import { useTasks } from "features/todolists/lib/lib/hooks/useTasks"
 import { DomainTodolist } from "features/todolists/lib/types/types"
 import { TasksSkeleton } from "features/todolists/ui/skeletons/TasksSkeleton/TasksSkeleton"
-import { useState } from "react"
 import { TasksPagination } from "../TasksPagination/TasksPagination"
 import { Task } from "./Task/Task"
 
@@ -12,34 +10,24 @@ type Props = {
 }
 
 export const Tasks = ({ todolist }: Props) => {
-  const [page, setPage] = useState(1)
-
-  const { data, isLoading } = useGetTasksQuery({ todolistId: todolist.id, args: { page } })
+  const { tasks, isLoading, totalCount, page, setPage } = useTasks(todolist)
 
   if (isLoading) {
     return <TasksSkeleton />
   }
-  let tasksForTodolist = data?.items
 
-  if (todolist.filter === "active") {
-    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.New)
-  }
-
-  if (todolist.filter === "completed") {
-    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.Completed)
-  }
   return (
     <>
-      {tasksForTodolist?.length === 0 ? (
+      {tasks?.length === 0 ? (
         <p>Тасок нет</p>
       ) : (
         <>
           <List>
-            {tasksForTodolist?.map((task) => {
+            {tasks?.map((task) => {
               return <Task key={task.id} task={task} todolist={todolist} />
             })}
           </List>
-          <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />
+          <TasksPagination totalCount={totalCount || 0} page={page} setPage={setPage} />
         </>
       )}
     </>
